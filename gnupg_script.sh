6aa4f7931a4f33fab40377204055433a012fe5b8
ashweeps <asespinozaro@uide.edu.ec>
2026-03-20 18:05:57 +0000

    generator

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,5 +1,6 @@
 uname_a # Da la información del ordenador 
 which gpg # Da la ubicación del binaro de gpg
 gpg --version #Nos dice la version del gpg e información adicional 
-gpg --full-generate-key #para generar para de llaves 
-
+gpg --full-generate-key #para generar llave pública y la llave privada 
+gpg --list-keys #Para ver las llaves
+gpg -armor --export #Para exportar las llaves 
