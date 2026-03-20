e997369af93c9efb705bcb2d9e3ec7fab1fdb7a9
ashweeps <asespinozaro@uide.edu.ec>
2026-03-20 18:00:23 +0000

    4

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,2 +1,5 @@
 uname_a # Da la información del ordenador 
 which gpg # Da la ubicación del binaro de gpg
+gpg --version #Nos dice la version del gpg e información adicional 
+gpg --full-generate-key #para generar para de llaves 
+
