6661aa787339f521c6f2d67130716349e7e2ebfc
ashweeps <asespinozaro@uide.edu.ec>
2026-04-16 12:46:56 +0000

    ls-lah

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -1,3 +1,9 @@
 #Shows all files in the directory, including hidden ones.
 #It does the same thing as ls -a, it's the long form of the command
 ls -a = ls -all
+#It displays file sizes in a readable format, for example: KB, MB, GB.
+ls -h
+#It displays a detailed list of all files, including hidden ones with sizes in a readable format.
+ls -l -a -h
+#It displays all files in the directory, including hidden ones, in list format, with detailed information such as permissions, size, and date, using easy-to-read formats. It's the same as the previous command.
+ls -lah
