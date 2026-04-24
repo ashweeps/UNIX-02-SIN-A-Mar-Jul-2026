5623f7d07c5438b82f819224207acc395a398aef
ashweeps <asespinozaro@uide.edu.ec>
2026-04-24 17:52:15 +0000

    problem4

--- history.sh
+++ history.sh
@@ -103,3 +103,56 @@ Problem 2:
    35  chmod 600 registros/sensores.log
    36  ls -l registros/sensores.log
    37  history
+
+Problem 4
+    1  git fetch upstream
+    2  git remote -v
+    3  git checkout -b eval_p2_1_g2 upstream/eval_p2_1_g2
+    4  git push -u origin eval_p2_1_g2
+    5  history
+    6  cd evaluation
+    7  cd kepler
+    8  mv kepler kepler/registros
+    9  mv kepler_docs  kepler/registros
+   10  mv kepler kepler/registros}
+   11  mv kepler/docs kepler/registros
+   12  cd kepler
+   13  cd evaluation
+   14  ls
+   15  mv kepler/docs kepler/registros
+   16  mv kepler_docs kepler/registros
+   17  cd evaluation
+   18  cd kepler
+   19  git push -u origin eval_p2_1_g2
+   20  cd kepler
+   21  git push -u origin eval_p2_1_g2
+   22  cd evaluation
+   23  git push -u origin eval_p2_1_g2
+   24  cd evaluation
+   25  ls -a
+   26  cd evaluation
+   27  cd kepler
+   28  mv docs registros
+   29  mkdir plantillas
+   30  history
+   31  mv sensores.log registros/
+   32  history
+   33  git add .
+   34  git commit -m "Fix problema 2: move archive and rename"
+   35  chmod 600 registros/sensores.log
+   36  ls -l registros/sensores.log
+   37  history
+   38  git add .
+   39  git commit -m "Fix problema 3:numeric permissions 600 in sensors.log"}
+   40  git commit -m "Fix problema 3:numeric permissions 600 in sensors.log"
+   41  chmod u+x runner.sh
+   42  chmod o-w ajustes.conf
+   43  chmod o-w ajustes.old
+   44  ls -l runner
+   45  ls -l runnersh
+   46  ls -l runner.sh
+   47  ls -l ajustes-old
+   48  ls -l ajustes.old
+
+   Problem 5
+   
