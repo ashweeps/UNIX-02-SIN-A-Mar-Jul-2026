4c8536a82bab3cfce6b9585f587e6fc8bfa05210
ashweeps <asespinozaro@uide.edu.ec>
2026-04-24 17:44:54 +0000

    problem 2

--- history.sh
+++ history.sh
@@ -30,4 +30,37 @@ Problem 1:
    29  mkdir plantillas
    30  history
 
+Problem 2: 
+    1  git fetch upstream
+    2  git remote -v
+    3  git checkout -b eval_p2_1_g2 upstream/eval_p2_1_g2
+    4  git push -u origin eval_p2_1_g2
+    5  history
+    6  cd evaluation
+    7  cd kepler
+    8  mv kepler kepler/registros
+    9  mv kepler_docs  kepler/registros
+   10  mv kepler kepler/registros}
+   11  mv kepler/docs kepler/registros
+   12  cd kepler
+   13  cd evaluation
+   14  ls
+   15  mv kepler/docs kepler/registros
+   16  mv kepler_docs kepler/registros
+   17  cd evaluation
+   18  cd kepler
+   19  git push -u origin eval_p2_1_g2
+   20  cd kepler
+   21  git push -u origin eval_p2_1_g2
+   22  cd evaluation
+   23  git push -u origin eval_p2_1_g2
+   24  cd evaluation
+   25  ls -a
+   26  cd evaluation
+   27  cd kepler
+   28  mv docs registros
+   29  mkdir plantillas
+   30  history
+   31  mv sensores.log registros/
+   32  history
    
