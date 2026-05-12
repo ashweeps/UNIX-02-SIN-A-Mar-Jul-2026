fbc3fc58775f300458a0d20d6b857a4c28ee96b1
ashweeps <asespinozaro@uide.edu.ec>
2026-05-12 16:59:52 +0000

    Changing permissions

---
+++ security.zsh
@@ -0,0 +1,16 @@
+#Shows or changes the default file permissions for new files and directories.
+umask
+#Creates a new empty file
+touch
+#Creates a new directory
+mkdir 
+#Updates the package list from the repositories.
+apt-get update
+#Upgrades installed packages to newer versions.
+apt updgrade
+#Installs the ACL package for advanced permissions management.
+apt-get install acl
+#Changes the owner of the current directory and all its contents to the current user.
+chown -R $(whoami).
+#Removes all ACL permissions from the current directory and its contents recursively.
+setfacl -bnR .
