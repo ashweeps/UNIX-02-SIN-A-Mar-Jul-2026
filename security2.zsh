2331a9f63ac344ba881b90b40ab44bbead23b560
ashweeps <asespinozaro@uide.edu.ec>
2026-05-28 13:24:31 +0000

    directory creation practice

--- security2.zsh
+++ security2.zsh
@@ -13,13 +13,13 @@ echo "Grupo actual: $(id -gn)"
 #Creates an empty file named antes_de_newgrp.txt in the home directory.
 touch ~/antes_de_newgrp.txt
 #Displays detailed information about the file, including permissions, owner, group, size, and modification date.
-ls -la ~/antes_de_newgrp.txt
-#Downloads and installs the necessary packages for Linux utilities and group management, including util-linux-extra and uidmap.
-apt install util-linux-extra uidmap
+ls -la ~/antes_de_newgrp.
 #Updates the package list from the Kali Linux repositories to check for the latest available versions.
 apt update
 #Checks for available package upgrades and updates installed packages if needed.
 apt upgrade
+#Downloads and installs the necessary packages for Linux utilities and group management, including util-linux-extra and uidmap.
+apt install util-linux-extra uidmap
 #Creates a new group called desarrolladores.
 groupadd desarrolladores
 #Changes the current active group to desarrolladores.
@@ -28,3 +28,9 @@ newgrp desarrolladores
 id -gn
 #Displays a message showing the new active group using command substitution.
 echo "Nuevo grupo activo: $(id -gn)"
+#Creates an empty file named dentro_de_newgrp.txt inside the home directory while the active group is desarrolladores.
+touch ~/dentro_de_newgrp.txt
+#Displays detailed information about the file, including permissions, owner, active group, size, and modification date.
+ls -la ~/dentro_de_newgrp.txt
+#Creates the directory proyecto_dev/src and any missing parent directories using the -p option.
+mkdir -p ~/proyecto_dev/src
