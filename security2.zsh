2f64b46b44dfde0c910c1a35deb0867b54a4b849
ashweeps <asespinozaro@uide.edu.ec>
2026-05-28 12:55:12 +0000

    add newgrp and group ownership

--- security2.zsh
+++ security2.zsh
@@ -6,3 +6,11 @@ id -gn
 touch ~/test_grupo_heredado.txt
 #Displays detailed information about the file, including permissions, owner, group, size, and last modification date.
 ls -la ~/test_grupo_heredado.
+#Shows the name of the current user’s primary group.
+id -gn
+#Displays a message showing the current group by using command substitution with $(id -gn).
+echo "Grupo actual: $(id -gn)"
+#Creates an empty file named antes_de_newgrp.txt in the home directory.
+touch ~/antes_de_newgrp.txt
+#Displays detailed information about the file, including permissions, owner, group, size, and modification date.
+ls -la ~/antes_de_newgrp.txt
