9d4ddf83e0d1aa90c18f203a9d78e802e5a8d39d
ashweeps <asespinozaro@uide.edu.ec>
2026-05-28 12:51:54 +0000

    file group tests

---
+++ security2.zsh
@@ -0,0 +1,8 @@
+#Displays the current user’s information, including the user ID (UID), group ID (GID), and all the groups the user belongs to.
+id
+#Shows the name of the current user’s primary group.
+id -gn
+#Creates an empty file named test_grupo_heredado.txt in the home directory.
+touch ~/test_grupo_heredado.txt
+#Displays detailed information about the file, including permissions, owner, group, size, and last modification date.
+ls -la ~/test_grupo_heredado.
