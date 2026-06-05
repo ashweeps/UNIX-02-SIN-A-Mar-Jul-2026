ebebbcaee2407a2b9b1aac515ab548bcf59e10ed
ashweeps <asespinozaro@uide.edu.ec>
2026-06-05 16:38:24 +0000

    monitor processes

---
+++ blackhatbash5.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+#Shows active processes in real time, including CPU and memory usage.
+top
+#Runs the top command in the background, so the terminal stays available for other commands.
+top &
