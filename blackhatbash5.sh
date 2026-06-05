150353f58ce9b803911d3c2ccfd2b81d2b89d23a
ashweeps <asespinozaro@uide.edu.ec>
2026-06-05 17:16:18 +0000

    practice output errors

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,19 @@
 top
 #Runs the top command in the background, so the terminal stays available for other commands.
 top &
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "El comando lzl falló"
+echo "Hello World!" > output.txt
+cat output.txt
+cho "Chao mundo!" > output.txt
+cat output.tx
+echo "Chao chao!">> output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
