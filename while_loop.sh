132967ea558d42c227179336539623757747c547
ashweeps <asespinozaro@uide.edu.ec>
2026-06-12 16:37:38 +0000

    explains the while loop

--- while_loop.sh
+++ while_loop.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+#Stores the name of the stop file in a variable.
 SIGNAL_TO_STOP_FILE="stoploop"
+
+#Starts a while loop that keeps running while the stoploop file does not exist.
 while [[ ! -f "${SIGNAL_TO_STOP_FILE}" ]]; do
-echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exis
-t..."
+
+#Prints a message saying that the stoploop file has not been created yet.
+echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exist..."
+
+#Prints a message saying the script will check again after 2 seconds.
 echo "Checking again in 2 seconds..."
+
+#Pauses the script for 2 seconds before repeating the loop.
 sleep 2
+
+#Ends the while loop.
 done
+
+#Prints this message when the stoploop file is found.
 echo "File was found! Exiting..."
