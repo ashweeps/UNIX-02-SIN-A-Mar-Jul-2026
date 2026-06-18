d0db14194ddec1a993aa21cd28d884b5181dfd35
ashweeps <asespinozaro@uide.edu.ec>
2026-06-18 13:05:03 +0000

    add comma-separated-awk

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -13,3 +13,15 @@ awk '{print $1,$2,$3}' log.txt
 awk '{print $2}' log.txt
 awk '{print $3}' log.txt
 awk '{print $1,$NF}' log.txt
+
+touch example_csv.txt
+#Creates the CSV file and writes each comma-separated line into it.
+echo "Hola,Ashley,cami" > example_csv.txt
+
+#Adds the first data line to the CSV file.
+echo "Taylor,Swift,Quito" >> example_csv.txt
+
+#Adds the second data line to the CSV file.
+echo "Holaa,Ashley,Pau" >> example_csv.txt
+
+awk -F',' '{print $1}' example_csv.txt
