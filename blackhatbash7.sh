444218c5c571570522399ce0f7a49115fc998659
ashweeps <asespinozaro@uide.edu.ec>
2026-06-19 16:55:38 +0000

    edit and compare log files

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -35,3 +35,7 @@ grep "42.236.10.117" log.txt | awk '{print $7}'
 sed 's/Mozilla/Godzilla/g' log.txt
 grep "Mozilla" log.txt
 grep "Godzilla" log.txt
+
+sed '1d' newlog.txtI
+sed -i '1d' newlog.txt
+diff log.txt newlog.tx
