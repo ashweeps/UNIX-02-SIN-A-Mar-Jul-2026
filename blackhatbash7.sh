e4f26d684231457a8becb368cc96b358dda7410d
ashweeps <asespinozaro@uide.edu.ec>
2026-06-19 17:04:52 +0000

    remove spaces and lines

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -38,4 +38,11 @@ grep "Godzilla" log.txt
 
 sed '1d' newlog.txtI
 sed -i '1d' newlog.txt
-diff log.txt newlog.tx
+diff log.txt newlog.txt
+
+sed '37d' newlog.txtI
+sed -i '37d' newlog.txt
+diff log.txt newlog.txt
+
+sed 's/ //g' log.txt > newlog1.txt
+sed '8,10d' newlog1.txt
